bdd65fbb442b6741dec6e9c62dfb87cce17aca72
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-02-03 13:25:15 -0700

    finished

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,41 @@
 import React from 'react'
+import sight from './sight.png'
+import taste from './taste.png'
+import smell from './smell.png'
+import sound from './sound.png'
+import touch from './touch.png'
 
 const Footer = () => {
     return <footer className='footer'>
         <div className='circles'>
-            {/* img */}
+            <img 
+                src={sight} 
+                alt='eye'
+                className='img'/>
         </div>
         <div className='circles'>
-            {/* img */}
+            <img 
+                src={taste} 
+                alt='tongue'
+                className='img'/>
         </div>
         <div className='circles'>
-            {/* img */}
+            <img 
+                src={smell} 
+                alt='nose'
+                className='img'/>
         </div>
         <div className='circles'>
-            {/* img */}
+            <img 
+                src={sound} 
+                alt='ear'
+                className='img'/>
         </div>
         <div className='circles'>
-            {/* img */}
+            <img 
+                src={touch} 
+                alt='hand'
+                className='img'/>
         </div>
     </footer>
 }
